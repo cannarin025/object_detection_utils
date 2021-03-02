cc62b9c7610874ae4ea00fef6f8fa784f1e41973
Can Narin <cannarin025@gmail.com>
2021-03-02 00:45:18 +0000

    Converted into functions for ease of use

--- rename_label/rename_label.py
+++ rename_label/rename_label.py
@@ -1,18 +1,24 @@
 import os
 
-dir_path = "L://Code//RealTimeObjectDetection//Tensorflow//workspace//images//test//"
+def rename_label(dir_path, target, replace):
 
+
+    for filename in os.listdir(dir_path):
+        if filename.split(".")[-1] == "xml":
+            filepath = dir_path + filename
+            with open(filepath, "r") as fp:
+                new = fp.read().replace(f"<name>{target}</name>",f"<name>{replace}</name>")  # old label --> new label 
+            
+            with open(filepath, "w+") as fp:
+                fp.write(new)
+
+    print("Done!")
+    return
+
+
+dir_path = "L://Code//cat_imgs//yolo//cats_labelled//"
 target = "car"
 replace = "cat"
 
-for filename in os.listdir(dir_path):
-    if filename.split(".")[-1] == "xml":
-        filepath = dir_path + filename
-        with open(filepath, "r") as fp:
-            new = fp.read().replace(f"<name>{target}</name>",f"<name>{replace}</name>")  # old label --> new label 
-        
-        with open(filepath, "w+") as fp:
-            fp.write(new)
-
-print("Done!")
+rename_label(dir_path, target, replace)
         
